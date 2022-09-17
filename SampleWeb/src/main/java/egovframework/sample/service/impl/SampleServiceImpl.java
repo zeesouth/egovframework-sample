289fb384a82f9213cead838ae567af71002a0322
남지수 <anm0307@naver.com>
2022-09-17 16:51:22 +0900

    change

--- SampleWeb/src/main/java/egovframework/sample/service/impl/SampleServiceImpl.java
+++ SampleWeb/src/main/java/egovframework/sample/service/impl/SampleServiceImpl.java
@@ -1,27 +1,36 @@
 package egovframework.sample.service.impl;
 
-public class SampleServiceImpl {
+import egovframework.sample.service.SampleService;
+
+public class SampleServiceImpl implements SampleService {
+	
+
 	
 	public SampleServiceImpl() throws Exception {
 		System.out.println("===> SampleServiceImpl 생성");
 	}
 	
+	@Override
 	public void insertSample() throws Exception {
 		System.out.println("SampleSevice--Sample 등록");
 	}
 	
+	@Override
 	public void updateSample() throws Exception {
 		System.out.println("SampleSevice--Sample 수정");
 	}
 	
+	@Override
 	public void deleteSample() throws Exception {
 		System.out.println("SampleSevice--Sample 삭제");
 	}
 	
+	@Override
 	public void selectSample() throws Exception {
 		System.out.println("SampleSevice--Sample 상세 조회");
 	}
 	
+	@Override
 	public void selectSampleList() throws Exception {
 		System.out.println("SampleSevice--Sample 목록 검색");
 	}
--- SampleWeb/src/test/java/egovfamework/example/sample/service/SampleServiceClient.java
+++ SampleWeb/src/test/java/egovfamework/example/sample/service/SampleServiceClient.java
@@ -3,12 +3,22 @@ package egovfamework.example.sample.service;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import egovframework.sample.service.SampleService;
+
 public class SampleServiceClient {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		AbstractApplicationContext container = new GenericXmlApplicationContext(
 				"egovframework/spring/context-common.xml");
+		
+		SampleService sampleService 
+			= (SampleService) container.getBean("sampleService");
+		sampleService.insertSample();
+		sampleService.selectSampleList();
+		
+		container.close();
+		
 	}
 
 }
